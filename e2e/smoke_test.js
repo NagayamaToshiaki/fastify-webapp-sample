77ec7c4da82c75a7afba5ed128ba22916e82bd98
ToshiakiNagayama <yr4hjdb6@i.softbank.jp>
2025-02-16 21:18:37 +0900

    URLを環境変数から渡すようにした

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,7 +1,7 @@
 SuiteOf('スモークテスト');
 
 Scenario('Webサイトを開きログインする', ({I}) => {
-    I.amOnPage('https://fastify-webapp-sample-production-0c7e.up.railway.app')
+    I.amOnPage('/')
     I.click('ログインする')
     I.fillField('ユーザー名', 'user1')
     I.fillField('パスワード', 'super-strong-passphrase')
